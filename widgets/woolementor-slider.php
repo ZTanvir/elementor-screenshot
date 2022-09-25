d34f2eed97aac347fca40ec42c30f9458bc834ee
Ferdowsi Marufa <marufa.fm@gmail.com>
2022-09-25 21:38:04 +0600

    img added by loop

--- widgets/woolementor-slider.php
+++ widgets/woolementor-slider.php
@@ -40,55 +40,31 @@ class Elementor_Hello_World_Widget_2 extends \Elementor\Widget_Base {
 			$all_src[$i] = $data['screenshots'][$i]["src"];
 		}
 				 
-		echo ' 
-		<div class="slideshow-container">
+		$i=0;
+		foreach( $all_src as $data) {
+	   $i++;	 
+		  echo ' 
+   <div class="slideshow-container">
 
-	   <!-- Full-width images with number and caption text -->
-	   
-	 
-	   <div class="mySlides fade">
-		 <div class="numbertext"> </div>
-		 <img src="'.$all_src[1].'" style="width:100%">
-		 <div class="text">Caption Two</div>
-	   </div>
-	 
-	   <div class="mySlides fade">
-		 <div class="numbertext"> </div>
-		 <img src="'.$all_src[2].'" style="width:100%">
-		 <div class="text">Caption Three</div>
-	   </div>
-	   <div class="mySlides fade">
-		 <div class="numbertext"></div>
-		 <img src="'.$all_src[3].'" style="width:100%">
-		 <div class="text">Caption Three</div>
-	   </div>
-	   <div class="mySlides fade">
-		 <div class="numbertext"> </div>
-		 <img src="'.$all_src[4].'" style="width:100%">
-		 <div class="text">Caption Three</div>
-	   </div>
-	   <div class="mySlides fade">
-		 <div class="numbertext"> </div>
-		 <img src="'.$all_src[5].'" style="width:100%">
-		 <div class="text">Caption Text</div>
-	   </div>
-	 
-	   <!-- Next and previous buttons -->
+		 <!-- Full-width images with number and caption text -->
+   
+	<div class="mySlides fade">
+		<div class="numbertext"> </div>
+		<img src="'.$data[$i].'" style="width:100%">
+	</div>
+	
+	 <!-- Next and previous buttons -->
 	   <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
 	   <a class="next" onclick="plusSlides(1)">&#10095;</a>
-	 </div>
-	 <br>
+   </div>
+   <br>
+   
+   <!-- The dots/circles -->
+   <div style="text-align:center">
+	 <span class="dot" onclick="currentSlide($i)"></span>
+   </div>'
+   ;}
 	 
-	 <!-- The dots/circles -->
-	 <div style="text-align:center">
-	   <span class="dot" onclick="currentSlide(1)"></span>
-	   <span class="dot" onclick="currentSlide(2)"></span>
-	   <span class="dot" onclick="currentSlide(3)"></span>
-	   <span class="dot" onclick="currentSlide(4)"></span>
-	   <span class="dot" onclick="currentSlide(5)"></span>
-	 </div>';
-	   
-
 		
    } 
 	   
