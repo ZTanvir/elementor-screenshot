ff18d973bf5fe3e034602a10bd67bc4bc454311d
Ferdowsi Marufa <marufa.fm@gmail.com>
2022-09-26 09:45:05 +0600

    img added without loop

--- widgets/woolementor-slider.php
+++ widgets/woolementor-slider.php
@@ -40,30 +40,55 @@ class Elementor_Hello_World_Widget_2 extends \Elementor\Widget_Base {
 			$all_src[$i] = $data['screenshots'][$i]["src"];
 		}
 				 
-		$i=0;
-		foreach( $all_src as $data) {
-	   $i++;	 
-		  echo ' 
-   <div class="slideshow-container">
+		echo '
+			 <!-- Slideshow container -->
+			 <div class="slideshow-container">
+			 
+			   <!-- Full-width images with number and caption text -->
+			   <div class="mySlides fade">
+				 <div class="numbertext"> </div>
+				 <img src="'.$all_src[1].'" style="width:100%">
+				 
+			   </div>
+			 
+			   <div class="mySlides fade">
+				 <div class="numbertext"> </div>
+				 <img src="'.$all_src[2].'" style="width:100%">
+		 
+			   </div>
+			 
+			   <div class="mySlides fade">
+				 <div class="numbertext"> </div>
+				 <img src="'.$all_src[3].'" style="width:100%">
+				 
+			   </div>
+			   <div class="mySlides fade">
+			   <div class="numbertext"> </div>
+			   <img src="'.$all_src[4].'" style="width:100%">
+			    
+			 </div>
+			 <div class="mySlides fade">
+			 <div class="numbertext"> </div>
+			 <img src="'.$all_src[5].'" style="width:100%">
+	 
+		   </div>
+			 
+			   <!-- Next and previous buttons -->
+			   <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+			   <a class="next" onclick="plusSlides(1)">&#10095;</a>
+			 </div>
+			 <br>
+			 
+			 <!-- The dots/circles -->
+			 <div style="text-align:center">
+			   <span class="dot" onclick="currentSlide(1)"></span>
+			   <span class="dot" onclick="currentSlide(2)"></span>
+			   <span class="dot" onclick="currentSlide(3)"></span>
+			   <span class="dot" onclick="currentSlide(4)"></span>
+			   <span class="dot" onclick="currentSlide(5)"></span>
+			 </div>';
 
-		 <!-- Full-width images with number and caption text -->
-   
-	<div class="mySlides fade">
-		<div class="numbertext"> </div>
-		<img src="'.$data[$i].'" style="width:100%">
-	</div>
-	
-	 <!-- Next and previous buttons -->
-	   <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-	   <a class="next" onclick="plusSlides(1)">&#10095;</a>
-   </div>
-   <br>
-   
-   <!-- The dots/circles -->
-   <div style="text-align:center">
-	 <span class="dot" onclick="currentSlide($i)"></span>
-   </div>'
-   ;}
+
 	 
 		
    } 
